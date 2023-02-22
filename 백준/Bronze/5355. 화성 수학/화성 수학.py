6860945e891c373edf16ce92a3646c4068d8b67f
Upsid3downWorld <115852982+Upsid3downWorld@users.noreply.github.com>
2023-02-23 08:29:10 +0900

    [Bronze II] Title: 화성 수학, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5355. 화성 수학/화성 수학.py
@@ -0,0 +1,18 @@
+T = int(input())
+
+for _ in range(T) :
+    cal = list(map(str, input().split()))
+    ans = 0
+
+    for i in range(len(cal)) :
+        if ( i == 0 ) :
+            ans += float(cal[i])
+        else :
+            if ( cal[i] == '@' ) :
+                ans *= 3
+            elif  ( cal[i] == '%' ) :
+                ans += 5
+            elif ( cal[i] == '#' ) :
+                ans -= 7
+
+    print('%0.2f' %ans)
