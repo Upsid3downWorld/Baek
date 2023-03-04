5427b80bd7941447da7bac5831f5794150a22e5f
Upsid3downWorld <115852982+Upsid3downWorld@users.noreply.github.com>
2023-03-04 22:13:49 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,6 +1,12 @@
-import math
+N1, N2 = map(int, input().split())
 
-N1, N2 = map(int,input().split())
+def get_gcd(N1, N2) :
+    while N2 > 0 :
+        N1, N2 = N2, N1 % N2
+    return N1
 
-print(math.gcd(N1, N2))
-print(math.lcm(N1, N2))
+def get_lcm(N1, N2) :
+    return (N1*N2) // (get_gcd(N1, N2))
+
+print(get_gcd(N1, N2))
+print(get_lcm(N1, N2))
