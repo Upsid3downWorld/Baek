2f4068d11a251b0a5db565263cc9bc545c8f300b
Upsid3downWorld <115852982+Upsid3downWorld@users.noreply.github.com>
2023-03-06 00:20:01 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -1,9 +1,9 @@
 sen = []
 
 A, B = map(int,input().split())
-for i in range(B+1) :
-   for j in range(i) :
-       if len(sen) == B :
-           break
-       sen.append(i)
+for i in range(1, B+1) :
+    for _ in range(i) :
+        if len(sen) == B :
+            break
+        sen.append(i)
 print(sum(sen[A-1:B]))
